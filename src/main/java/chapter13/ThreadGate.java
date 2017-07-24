7a408f28a130e89928d099be7ddb4fad2c57a1f6
hzguantao <hzguantao@corp.netease.com>
2017-07-24 09:48:38 +0800

    新增使用 wait 和 notifyAll 来实现可重新关闭的阀门

---
+++ src/main/java/chapter13/ThreadGate.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package chapter13;
+
+/**
+ * 使用 wait 和 notifyAll 来实现可重新关闭的阀门
+ */
+public class ThreadGate {
+	private boolean isOpen;
+	private int generation;
+
+	public synchronized void close() {
+		isOpen = true;
+	}
+
+	public synchronized void opean() {
+		++generation;
+		isOpen = true;
+		notifyAll();
+	}
+
+	public synchronized void await() throws InterruptedException {
+		int arrayvlGeneration = generation;
+
+		while (!isOpen && arrayvlGeneration == generation) {
+			wait();
+		}
+	}
+}
